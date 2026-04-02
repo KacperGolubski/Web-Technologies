5da4cc21800ac171defafdb0aee9ace02fe8cd12
KacperGolubski <mkacpergolubski@gmail.com>
2026-04-02 19:31:17 +0200

    Adding .js script to generate seats

--- sala_kinowa/index.js
+++ sala_kinowa/index.js
@@ -1,1 +1,15 @@
-
+const seatsContainer = document.getElementById("seats")
+let seats = [];
+for(let i = 1; i <= 150; i++) {
+    seats.push({id: i, status: "free"});
+    const seatElement = document.createElement("div");
+    seatElement.classList.add("seat", "free");
+    seatElement.textContent = i;
+    seatsContainer.appendChild(seatElement);
+}
+console.log("Cinema seats:", seats);
+const form = document.getElementById("form");
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log("Form submitted");
+});
