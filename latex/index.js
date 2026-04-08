80d905bc1e366e18f44b3280011ff96fc8ef3757
KacperGolubski <mkacpergolubski@gmail.com>
2026-04-08 12:24:00 +0200

    Adding dynamic change of matrix input

--- latex/index.js
+++ latex/index.js
@@ -1,3 +1,34 @@
+const matrixSizeSelect = document.getElementById("matrix_size");
+const matrixFieldsContainer = document.getElementById("matrix_fields");
+
+matrixSizeSelect.addEventListener("change", function() {
+    let size = matrixSizeSelect.value;
+
+    if (size === "2") {
+        matrixFieldsContainer.innerHTML = `
+            <input type="number" id="m00" value="1">
+            <input type="number" id="m01" value="2">
+            <br>
+            <input type="number" id="m10" value="3">
+            <input type="number" id="m11" value="4">
+        `;
+    } else if (size === "3") {
+        matrixFieldsContainer.innerHTML = `
+            <input type="number" id="m00" value="1">
+            <input type="number" id="m01" value="2">
+            <input type="number" id="m02" value="3">
+            <br>
+            <input type="number" id="m10" value="4">
+            <input type="number" id="m11" value="5">
+            <input type="number" id="m12" value="6">
+            <br>
+            <input type="number" id="m20" value="7">
+            <input type="number" id="m21" value="8">
+            <input type="number" id="m22" value="9">
+        `;
+    }
+});
+
 const calculateBtn = document.getElementById("calculate");
 calculateBtn.addEventListener("click", function() {
     let m00 = parseFloat(document.getElementById("m00").value);
